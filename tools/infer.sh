47961481a9104e6937fb7d17760c4ac4cc2c3421
ZHS <com.zhs@gmail.com>
2022-07-05 01:35:53 +0800

    test

--- tools/infer.sh
+++ tools/infer.sh
@@ -11,7 +11,7 @@ module load anaconda3
 source activate
 conda deactivate
 conda activate paddle
-python setup.py install
+python ppdet/modeling/tests/test_architectures.py
 #python tools/infer.py -c configs/ppyolo/ppyolo_r50vd_dcn_1x_coco.yml -o use_gpu=true weights=cache/weights/ppyolo_r50vd_dcn_1x_coco.pdparams --infer_img=demo/000000014439.jpg
 
 #--gpus $CUDA_VISIBLE_DEVICES
