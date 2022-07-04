99dd50d4f62af3946e2b8b32fcbce3afb215ce5d
ZHS <com.zhs@gmail.com>
2022-07-04 21:47:31 +0800

    test

--- tools/infer.sh
+++ tools/infer.sh
@@ -7,6 +7,7 @@
 
 rm /seu_share/home/dijunyong/220205723/usr/local/cuda
 ln -s /seu_share/home/dijunyong/220205723/usr/local/cuda-10.2 /seu_share/home/dijunyong/220205723/usr/local/cuda
+export CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES
 
 module load anaconda3
 source activate
