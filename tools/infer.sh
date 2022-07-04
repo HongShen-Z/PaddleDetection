c3fa50c4f257980aa0dcaf4b763fd65a13e7d96d
ZHS <com.zhs@gmail.com>
2022-07-04 21:17:05 +0800

    test

---
+++ tools/infer.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#BSUB -J infer
+#BSUB -o %J.out
+#BSUB -e %J.err
+#BSUB -q gpu_v100
+#BSUB -gpu "num=1:mode=exclusive_process:aff=yes"
+
+module load anaconda3
+source activate
+conda deactivate
+conda activate paddle
+python infer.py -c configs/ppyolo/ppyolo_r50vd_dcn_1x_coco.yml \
+-o use_gpu=true weights=https://paddledet.bj.bcebos.com/models/ppyolo_r50vd_dcn_1x_coco.pdparams \
+--infer_img=demo/000000014439.jpg
+
+#--gpus $CUDA_VISIBLE_DEVICES
+#~/.cache/torch/checkpoints/yolov5s.pt
