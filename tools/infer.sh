5b8df5721eaf950302da914a80599a3a5c5d481c
ZHS <com.zhs@gmail.com>
2022-07-05 18:16:12 +0800

    test

--- tools/infer.sh
+++ tools/infer.sh
@@ -11,7 +11,8 @@ module load anaconda3
 source activate
 conda deactivate
 conda activate paddle
-python ppdet/modeling/tests/test_architectures.py
+python -m pip install paddlepaddle-gpu==2.3.0.post112 -f https://www.paddlepaddle.org.cn/whl/linux/mkl/avx/stable.html
+#python ppdet/modeling/tests/test_architectures.py
 #python tools/infer.py -c configs/ppyolo/ppyolo_r50vd_dcn_1x_coco.yml -o use_gpu=true weights=cache/weights/ppyolo_r50vd_dcn_1x_coco.pdparams --infer_img=demo/000000014439.jpg
 
 #--gpus $CUDA_VISIBLE_DEVICES
