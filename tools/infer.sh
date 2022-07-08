9a7ba4e23a38525066e800cc8827b60d5cebecae
ZHS <com.zhs@gmail.com>
2022-07-08 20:49:12 +0800

    deploy/infer test

--- tools/infer.sh
+++ tools/infer.sh
@@ -8,12 +8,13 @@
 export CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES
 export LD_LIBRARY_PATH=/seu_share/home/dijunyong/220205723/usr/local/TensorRT-8.0.0.3/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/seu_share/home/dijunyong/220205723/usr/local/cuda-11.2/lib64:$LD_LIBRARY_PATH
+source /seu_share/home/dijunyong/220205723/.bashrc
 
 module load anaconda3
 source activate
 conda deactivate
 conda activate paddle
-python setup.py install
+#python setup.py install
 python ppdet/modeling/tests/test_architectures.py
 #python tools/infer.py -c configs/ppyolo/ppyolo_r50vd_dcn_1x_coco.yml \
 #-o use_gpu=true weights=cache/weights/ppyolo_r50vd_dcn_1x_coco.pdparams \
