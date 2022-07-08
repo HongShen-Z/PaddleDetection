9a7547e17a514d03cd0ed5d637d8d4c39b90c1f2
ZHS <com.zhs@gmail.com>
2022-07-08 23:46:07 +0800

    deploy/infer test

--- tools/infer.sh
+++ tools/infer.sh
@@ -15,8 +15,10 @@ module load anaconda3
 source activate
 conda deactivate
 conda activate paddle
-python setup.py install
-python ppdet/modeling/tests/test_architectures.py
+from paddle.fluid import core
+echo $core.cudnn_version()
+#python setup.py install
+#python ppdet/modeling/tests/test_architectures.py
 #python tools/infer.py -c configs/ppyolo/ppyolo_r50vd_dcn_1x_coco.yml \
 #-o use_gpu=true weights=cache/weights/ppyolo_r50vd_dcn_1x_coco.pdparams \
 #--infer_img=demo/000000014439.jpg
